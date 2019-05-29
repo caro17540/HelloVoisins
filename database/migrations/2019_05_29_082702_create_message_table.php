d495bfc7312df3369d7c74a2558341c7dda0e887
joelc64 <joso17600@gmail.com>
2019-05-29 10:47:17 +0200

    creation table message

---
+++ database/migrations/2019_05_29_082702_create_message_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message', function (Blueprint $table) {
+            $table->bigIncrements('id');
+			$table->bigInteger('id_transaction');
+			$table->bigInteger('id_sender');
+			$table->bigInteger('id_receiver');
+			$table->string('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('message');
+    }
+}
