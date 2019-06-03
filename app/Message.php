24b84047239bb323b7240e5f079b13eaac42ff4b
CindyPuybareau <puybareaucindy@gmail.com>
2019-06-03 14:29:56 +0200

    Création des dépendances Message

--- app/Message.php
+++ app/Message.php
@@ -7,5 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     protected $table = 'message';
-    protected $primaryKey = 'id'; 
+    protected $primaryKey = 'id';
+    protected $fillable = ['text'];
+
+    public function messageTransaction()
+    {
+        return $this->belongsTo('App\Transaction', 'id_transaction');
+    }
+    public function userSender()
+    {
+        return $this->belongsTo('App\User', 'id_sender');
+    }
+    public function userReceiver()
+    {
+        return $this->belongsTo('App\User', 'id_receiver');
+    }
 }
