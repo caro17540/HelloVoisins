3573136dd7731416d137ce815bd66fba21a44375
caro17540 <50023943+caro17540@users.noreply.github.com>
2019-06-03 11:49:00 +0200

    Creation model Transaction

---
+++ app/Transaction.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    /**
+	 * The table associated with the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'transaction';
+
+	/**
+	 * The primary key associated with the table.
+	 *
+	 * @var string
+	 */
+	protected $primaryKey = 'id';
+	
+}
