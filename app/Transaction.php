6d2253f8bf930806a6fedb29bc9b96518160fef3
caro17540 <50023943+caro17540@users.noreply.github.com>
2019-06-03 14:28:40 +0200

    Création des dépendances avec le modèle Transaction

--- app/Transaction.php
+++ app/Transaction.php
@@ -20,4 +20,56 @@ class Transaction extends Model
 	 */
 	protected $primaryKey = 'id';
 	
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = ['status', 'status1', 'status2'];
+	
+	/**
+	 * Get the user1 of the transaction.
+     *
+	 */
+	public function user1()
+	{
+			return $this->belongsTo('App\User', 'id_user1', 'id');
+	}
+			
+	/**
+	 * Get the user2 of the transaction.
+     *
+	 */
+	public function user2()
+	{
+			return $this->belongsTo('App\User', 'id_user2', 'id');
+	}
+	
+	/**
+	 * Get the skill of the user1 of the transaction.
+     *
+	 */
+	public function skill1()
+	{
+			return $this->belongsTo('App\Skill', 'id_skill1', 'id');
+	}
+			
+	/**
+	 * Get the skill of the user2 of the transaction.
+     *
+	 */
+	public function skill2()
+	{
+			return $this->belongsTo('App\Skill', 'id_skill2', 'id');
+	}
+			
+	/**
+	 * Get the messages of the transaction.
+     *
+	 */
+	public function messages()
+	{
+			return $this->hasMany('App\Message', 'id_transaction', 'id');
+	}
+			
 }
