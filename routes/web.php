b369319cb99cfed55518723ad9e389f4ea4d78d8
caleirofrancis <siracuse3@hotmail.fr>
2019-06-04 17:16:53 +0200

    Grosse Modifications

--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,15 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/' , 'HomeController@index')->name('home');
-Route::get('/account'/,'AccountController@form');
-Route::post('account', 'AccountController@update');
-Route:: post('/users',UserController@list');
-Route::get('/transactions' , 'transactionController@list');
-Route::get('/user{n}' , 'userController@read');
-Route::get('/transactions' , 'transactionController@list');
-Route::get('/user{n}' , 'userController@read');
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/account', 'AccountController@form');
+Route::post('/account', 'AccountController@update');
+Route::get('/users', 'UserController@list');
+Route::get('/user/{n}', 'UserController@read');
+Route::get('/transactions', 'TransactionController@list');
+Route::get('/transaction/{n}', 'TransactionController@read');
 Route::get('/skills', 'SkillController@list');
-Route::get('skill/create','!SkillController@form');
-Route::get('/skill/upate/{n}','SkillController@form');
-Route::get('/skill/update/{n}',SkillController@update');
+Route::get('/skill/create', 'SkillController@form');
+Route::get('/skill/update/{n}', 'SkillController@form');
+Route::post('/skill/update/{n}', 'SkillController@update');
 
